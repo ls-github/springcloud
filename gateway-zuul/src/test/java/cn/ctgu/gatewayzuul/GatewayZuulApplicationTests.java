1a41bb5e49d69e143b2c70899af493bfd2a260d0
liushi <liushi@fiberhome.com>
2019-11-28 19:27:31 +0800

    Spring Boot Actuator

---
+++ gateway-zuul/src/test/java/cn/ctgu/gatewayzuul/GatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ctgu.gatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
