de0c0c52c33216e2597df59b758322bb082afcfe
xmy <1597076708@qq.com>
2019-11-30 14:37:36 +0800

    zuul网关

---
+++ gateway-zuul/src/main/java/cn/ctgu/gatewayzuul/filter/TokenFilter.java
@@ -0,0 +1,42 @@
+package cn.ctgu.gatewayzuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class TokenFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request=context.getRequest();
+        String token=request.getParameter("token");
+        if(StringUtils.isBlank(token)){
+            context.setResponseStatusCode(401);
+            context.setResponseBody("access with no token!");
+            return null;
+        }
+        return null;
+    }
+}
